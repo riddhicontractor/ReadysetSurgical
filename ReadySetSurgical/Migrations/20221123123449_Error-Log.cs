e876599da9bdb6f69b049c2aaa6c77b07ce12d29
Riddhi Contractor <riddhi.contractor@zymr.com>
2022-11-23 18:20:03 +0530

    Error log

---
+++ ReadySetSurgical/Migrations/20221123123449_Error-Log.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReadySetSurgical.Migrations
+{
+    /// <inheritdoc />
+    public partial class ErrorLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
