39bdc26fe04280bc9e4040a7407bea5c8fae501a
Riddhi Contractor <riddhi.contractor@zymr.com>
2022-12-02 11:18:15 +0530

    adding migrations

--- ReadySetSurgical/Program.cs
+++ ReadySetSurgical/Program.cs
@@ -1,10 +1,13 @@
 using Amazon.S3;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddAWSService<IAmazonS3>();
+string connectionString = "server=sample-instance.c53wji5mnp4g.ap-south-1.rds.amazonaws.com;User Id=Admin;Password=Jz7XXc8iqCHjJTL;database=sample;Trusted_Connection=True;TrustServerCertificate=Yes;Integrated Security=false;";
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connectionString));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
