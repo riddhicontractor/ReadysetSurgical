f6d872e04ce092c59f3a8198edb88004baf87d9f
Riddhi Contractor <riddhi.contractor@zymr.com>
2022-12-19 12:29:27 +0530

    RSS

--- ReadySetSurgical/Controllers/FileListController.cs
+++ ReadySetSurgical/Controllers/FileListController.cs
@@ -7,10 +7,9 @@ namespace ReadySetSurgical.Controllers
     public class FileListController : Controller
     {
         //private readonly SqlConnection _connection;
-        DataContext dataContext;
-
-        _dataContext = dataContext;
-        //public string connectionString = "server=sample-instance.c53wji5mnp4g.ap-south-1.rds.amazonaws.com;User Id=Admin;Password=Jz7XXc8iqCHjJTL;database=sample;Trusted_Connection=True;TrustServerCertificate=Yes;Integrated Security=false;";
+        //DataContext dataContext;
+        //readonly DataContext _dataContext = dataContext;
+        public string connectionString = "server=sample-instance.c53wji5mnp4g.ap-south-1.rds.amazonaws.com;User Id=Admin;Password=Jz7XXc8iqCHjJTL;database=sample;Trusted_Connection=True;TrustServerCertificate=Yes;Integrated Security=false;";
         List<InvoiceDetails> details = new List<InvoiceDetails>();
         List<ErrorLog> logs = new List<ErrorLog>();
         public IActionResult Index()
@@ -32,33 +31,39 @@ namespace ReadySetSurgical.Controllers
         {
             try
             {
-                //using (var conn = new SqlConnection(connectionString))
+                //using (var ctx = new DataContext())
                 //{
-                //    conn.Open();
-
-                //    SqlCommand cmd = new SqlCommand();
-                //    cmd.Connection = conn;
-
-                //    cmd.CommandText = "SELECT * FROM [sample].[dbo].[invoiceDetails]";
-
-                //    SqlDataReader dr;
-
-                //    dr = cmd.ExecuteReader();
-
-                //    while (dr.Read())
-                //    {
-                //        details.Add(new InvoiceDetails()
-                //        {
-                //            Id = Convert.ToInt32(dr["Id"]),
-                //            FileName = dr["FileName"].ToString(),
-                //            InvoiceNumber = dr["InvoiceNumber"].ToString(),
-                //            VendorName = dr["VendorName"].ToString(),
-                //            ReceiverName = dr["ReceiverName"].ToString(),
-                //            CreatedAt = Convert.ToDateTime(dr["CreatedAt"])
-                //        });
-                //    }
-                //    conn.Close();
+                //    var details = (from s in ctx.invoiceDetails
+                //                   where s.Id == 1
+                //                   select s).FirstOrDefault<InvoiceDetails>();
                 //}
+                using (var conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = conn;
+
+                    cmd.CommandText = "SELECT * FROM [sample].[dbo].[invoiceDetails]";
+
+                    SqlDataReader dr;
+
+                    dr = cmd.ExecuteReader();
+
+                    while (dr.Read())
+                    {
+                        details.Add(new InvoiceDetails()
+                        {
+                            Id = Convert.ToInt32(dr["Id"]),
+                            FileName = dr["FileName"].ToString(),
+                            InvoiceNumber = dr["InvoiceNumber"].ToString(),
+                            VendorName = dr["VendorName"].ToString(),
+                            ReceiverName = dr["ReceiverName"].ToString(),
+                            CreatedAt = Convert.ToDateTime(dr["CreatedAt"])
+                        });
+                    }
+                    conn.Close();
+                }
             }
             catch(Exception ex)
             {
@@ -70,30 +75,30 @@ namespace ReadySetSurgical.Controllers
         {
             try
             {
-                //using (var conn = new SqlConnection(connectionString))
-                //{
-                //    conn.Open();
+                using (var conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
 
-                //    SqlCommand cmd = new SqlCommand();
-                //    cmd.Connection = conn;
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = conn;
 
-                //    cmd.CommandText = "SELECT * FROM [sample].[dbo].[ErrorLog]";
+                    cmd.CommandText = "SELECT * FROM [sample].[dbo].[ErrorLog]";
 
-                //    SqlDataReader dr;
+                    SqlDataReader dr;
 
-                //    dr = cmd.ExecuteReader();
+                    dr = cmd.ExecuteReader();
 
-                //    while (dr.Read())
-                //    {
-                //        logs.Add(new ErrorLog()
-                //        {
-                //            Id = Convert.ToInt32(dr["Id"]),
-                //            FileName = dr["FileName"].ToString(),
-                //            CreatedAt = Convert.ToDateTime(dr["CreatedAt"])
-                //        });
-                //    }
-                //    conn.Close();
-                //}
+                    while (dr.Read())
+                    {
+                        logs.Add(new ErrorLog()
+                        {
+                            Id = Convert.ToInt32(dr["Id"]),
+                            FileName = dr["FileName"].ToString(),
+                            CreatedAt = Convert.ToDateTime(dr["CreatedAt"])
+                        });
+                    }
+                    conn.Close();
+                }
             }
             catch (Exception ex)
             {
