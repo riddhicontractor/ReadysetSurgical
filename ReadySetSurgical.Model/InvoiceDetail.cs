f43b178c1e0e03d19b85a018c8f813f441ad14a6
Riddhi Contractor <riddhi.contractor@zymr.com>
2022-12-28 17:12:09 +0530

    Added Class Library.

---
+++ ReadySetSurgical.Model/InvoiceDetail.cs
@@ -0,0 +1,12 @@
+﻿namespace ReadySetSurgical.Model
+{
+    public class InvoiceDetail
+    {
+        public int Id { get; set; }
+        public string InvoiceNumber { get; set; }
+        public string VendorName { get; set; }
+        public string ReceiverName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string FileName { get; set; }
+    }
+}
--- ReadySetSurgical/Data/DataContext.cs
+++ ReadySetSurgical/Data/DataContext.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using ReadySetSurgical.Models;
+using ReadySetSurgical.Model;
 
 namespace ReadySetSurgical.Data
 {
